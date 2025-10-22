188543e2b8e4c26aa168940d4c11978d57b163cd
tsartrooper <pratmane00007@gmail.com>
2025-10-22 14:02:13 +0530

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI();
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ployspeak.vercel.app/"],
+    allow_origins=["https://ployspeak.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
